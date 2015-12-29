fc6dfbcad6658ef4fcf7510ddaf64b7b777d24e3
calvin-fisher <calvin.fisher@gmail.com>
2015-12-29 10:56:01 -0600

    Added reference to grid

--- Assets/Logic/Scripts/TileManager.cs
+++ Assets/Logic/Scripts/TileManager.cs
@@ -6,8 +6,10 @@ public class TileManager : MonoBehaviour
     private const int Width = 100;
     private const int Length = 100;
 
-	// Use this for initialization
-	void Start ()
+    public GameObject Tile;
+
+    // Use this for initialization
+    void Start ()
 	{
 	    CreateTiles();
 	}
@@ -20,14 +22,15 @@ public class TileManager : MonoBehaviour
 
     private void CreateTiles()
     {
-        var minX = 0 - (Width/2);
-        var minY = 0 - (Width/2);
-        var maxX = minX + Width;
-        var maxY = minY + Length;
+        var minX = 0;
+        var minY = 0;
+        var maxX = Width;
+        var maxY = Length;
 
-        Debug.Log(string.Format("Drawing {0} x {1} grid. X({2} to {3}) Y({4} to {5})",
-            Width, Length, minX, maxX, minY, maxY));
+        Debug.Log(string.Format("Initializing {0} x {1} grid. X({2} to {3}) Y({4} to {5})",
+            Width, Length, minX, maxX - 1, minY, maxY - 1));
 
+        _grid = new Tile[Width,Length];
 
         for (int x = minX; x < maxX; x++)
         {
@@ -35,10 +38,16 @@ public class TileManager : MonoBehaviour
             {
                 var newTile = GameObject.Instantiate(Tile);
                 newTile.transform.position = new Vector3(x, 0, y);
+
+                var tile = newTile.GetComponent<Tile>();
+                _grid[x, y] = tile;
             }
         }
     }
 
-
-    public GameObject Tile;
+    private Tile[,] _grid;
+    public Tile Get(int x, int y)
+    {
+        return _grid[x, y];
+    }
 }
